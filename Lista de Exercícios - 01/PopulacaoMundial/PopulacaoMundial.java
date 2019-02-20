72b6dbd54f8fedeeba5cfd152438e8fa1ed16cab
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-02-20 20:45:13 -0300

    População Mundial + gitignore

--- Lista de Exercícios - 01/PopulacaoMundial/PopulacaoMundial.java
+++ Lista de Exercícios - 01/PopulacaoMundial/PopulacaoMundial.java
@@ -1,5 +1,18 @@
 public class PopulacaoMundial {
   public static void main(String[] args) {
-    
+    if (args.length != 1) {
+      System.out.println("Necessário passar um valor númerico como parâmetro");
+      System.exit(0);
+    }
+
+    Long POPULACAO_ATUAL = 7000000000L;
+    Double TAXA_CRESCIMENTO = 1.2;
+
+    Integer ano = Integer.valueOf(args[0]);
+
+    Double populacaoFinal = POPULACAO_ATUAL * Math.pow((1 + (TAXA_CRESCIMENTO / 100)), ano);
+    Long populacaoFinalLong = Math.round(populacaoFinal);
+
+    System.out.println(populacaoFinalLong + " pessoas");
   }
 }
