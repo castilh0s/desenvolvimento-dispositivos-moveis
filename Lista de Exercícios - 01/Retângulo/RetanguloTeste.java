9bb08e7c87994eee7656d9a5d9db5a94b0d1ab86
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-02-25 20:49:04 -0300

    Retangulo OO

--- Lista de Exercícios - 01/Retângulo/RetanguloTeste.java
+++ Lista de Exercícios - 01/Retângulo/RetanguloTeste.java
@@ -2,6 +2,12 @@
 public class RetanguloTeste {
 	public static void main(String[] args) throws Exception {
 		validate(args);
+
+		Double sideA = Double.valueOf(args[0]);
+		Double sideB = Double.valueOf(args[1]);
+
+		Retangulo retangulo = new Retangulo(sideA, sideB);
+		System.out.println(retangulo);
 	}
 
 	private static void validate(String[] args) throws Exception {
